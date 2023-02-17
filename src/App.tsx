4edd2e299272bf125e5db1a3c54ec495f39ba7f1
Tudor Teisanu <tudor.teisanu@powerit.dev>
2023-02-17 21:39:31 +0200

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BaseForm, FormInput } from "nanoit-react-components";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const form = useForm();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Login form</div>
+      <BaseForm form={form}>
+        <FormInput name={"test1"} label="Test"></FormInput>
+        <FormInput label="Test" name={"test2"}></FormInput>
+        <button type={"submit"}>Submit</button>
+      </BaseForm>
     </div>
   );
 }
