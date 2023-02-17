6fa6d39a16bca41ae871ad546fd43fbb9e58dd66
Tudor Teisanu <tudor.teisanu@powerit.dev>
2023-02-17 23:53:46 +0200

    setup components library

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import "./App.css";
-import { BaseForm, FormInput } from "nanoit-react-components";
+import { BaseForm, DataTable, FormInput } from "nanoit-react-components";
 import { useForm } from "react-hook-form";
 
 function App() {
   const form = useForm();
+  const submit = (data: any) => {
+    console.log(data);
+  };
   return (
     <div className="App">
       <div>Login form</div>
-      <BaseForm form={form}>
+      <BaseForm form={form} onSubmit={submit}>
         <FormInput name={"test1"} label="Test"></FormInput>
         <FormInput label="Test" name={"test2"}></FormInput>
         <button type={"submit"}>Submit</button>
       </BaseForm>
+      <DataTable
+        headers={[
+          { value: "id", text: "ID" },
+          { value: "name", text: "Name" },
+        ]}
+        items={[{ id: 1, name: "Pascal" }]}
+      />
     </div>
   );
 }
